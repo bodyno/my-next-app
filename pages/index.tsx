09d4ae7331d00e537861feedce6f1abaffdc8047
bodyno <az8321550@gmail.com>
2021-08-10 11:37:55 +0800

    feat: optimize

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Body's Next.js Site!
+          Welcome to Body Next.js Site!
         </h1>
 
         <p className={styles.description}>
